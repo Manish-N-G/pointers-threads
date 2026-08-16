66e00d8c9d81cd2fedba5bf069ef00b8536ef5bf
Manish-N-G-Zuko <136582364+Manish-N-G@users.noreply.github.com>
2026-08-16 23:25:33 +0200

    Remove comment to start on the current implementation.

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ fn main() {
     // thread1c_mutex_lock_attempt_drop(std::sync::Mutex::new(100u8), true, true, true);
     // thread1c_mutex_lock_attempt_drop(std::sync::Mutex::new(100u8), true, false, true);
     //
-    // thread1c_mutex_lock_attempt(true, 33); //todomanish: I was here
+    thread1c_mutex_lock_attempt(true, 33); 
     // thread1c_mutex_hangs_forever( 5, true );
     // crate::th3::th3b2::test_async_spawn();
     //
