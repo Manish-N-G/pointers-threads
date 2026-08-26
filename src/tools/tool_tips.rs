e1dd2c1102008632267b553c0ef68bc7ad5f2a54
Manish-N-G-Zuko <136582364+Manish-N-G@users.noreply.github.com>
2026-08-27 01:05:48 +0200

    Update doc file. Has more tools for our project

--- src/tools/tool_tips.rs
+++ src/tools/tool_tips.rs
@@ -4,6 +4,7 @@
 //! but It should encompass general testing, benching, performance options. Not to mention other
 //! specific traits that will help to.
 //!
+//! <br><br>
 //! ### Spelling checking
 //!
 //! - For Spelling we could use [Cargo Spellcheck](http://crates.io/crates/cargo-spellcheck)
@@ -27,9 +28,10 @@
 //! // see full list in the git repo
 //! ```
 //!
+//! <br><br>
 //! ### Some Cargo based tools
 //! - Cargo info and search: A tool to find out about a cargo package
-//! To simply use it as follows. cargo info however, can give use what 
+//! To simply use it as follows. cargo info however, can give use what
 //! features are available to the package.
 //! ```text
 //! cargo info bacon
@@ -52,7 +54,7 @@
 //! ```
 //! <img src="https://github.com/Manish-N-G/pointers-threads/blob/master/assets/seek.png?raw=true"></img>
 //!
-//! - Cargo Clippy: Clippy is quite powerful to linting and ignoring files that we want to 
+//! - Cargo Clippy: Clippy is quite powerful to linting and ignoring files that we want to
 //! ignore for linking errors. To make it work, we could provide some files to be ignored
 //! or linting. This is achieved via the `Cargo.toml` file and 'Clippy.toml' file. However
 //! they serve different purposes by default. See the online documentation to know more.
@@ -61,6 +63,7 @@
 //! cargo clippy
 //! ```
 //!
+//! <br><br>
 //! ### Testing Tools
 //! - Cargo tests and Cargo doc --test are some of the ways we can do testing, and we can
 //! test based on the path as well if needed.
@@ -85,11 +88,13 @@
 //! runs. It give us test coverage, allows for mutation based testing, even going to the
 //! point of observing system behaviour.
 //!
+//! <br><br>
 //! ### Benching Tools
 //! - We can use cargo bench to bench our code. This can be available with nextest as well
-//! to see how long it takes to complete. However bench in generally through of the 
-//! default. 
+//! to see how long it takes to complete. However bench in generally through of the
+//! default.
 //! ```text
+//! cargo bench
 //! ```
 //! However, there are other tools at our disposal, that can help us to perhaps get even
 //! better results that using cargo bench.
@@ -97,17 +102,65 @@
 //! package that gives us good test results and functionality that allows to test our
 //! project.
 //! ```text
+//! // will use the same command, expect it now depends on the cargo criterion modules
+//! cargo bench
+//!
+//! // before criterion, we would do something like
+//! #![feature(test)]
+//! extern crate test;
+//! use test::Bencher;
+//! use test::black_box;
+//! use rayon::prelude::*;
+//!
+//! fn get_incremental_sum_from_index_count( idx: u16, count: u64 ) -> u128 {
+//!     ((idx as u128)..=(idx as u128+count as u128)).into_par_iter().sum()
+//! }
+//! 
+//! // we just aadd the tag:
+//! #[bench]
+//! fn normal_bench(b: &mut Bencher) {
+//!     b.iter( || get_incremental_sum_from_index_count( 400, 423293393) );
+//!     b.iter( || get_incremental_sum_from_index_count( black_box(303), black_box(23423439)) );
+//! }
+//!
+//! ```
+//! for criterion
+//! ```
+//! // this can actually be run, but we dont assert anything with it
+//! use criterion::{criterion_group, criterion_main, Criterion};
+//! use std::hint::black_box;
+//! use rayon::prelude::*;
+//!
+//! fn get_incremental_sum_from_index_count( idx: u16, count: u64 ) -> u128 {
+//!     ((idx as u128)..=(idx as u128+count as u128)).into_par_iter().sum()
+//! }
+//!
+//! fn crit_bench(c: &mut Criterion) -> &mut Criterion {
+//!     c.bench_function("inc to count from idx", |b| {
+//!         b.iter( || get_incremental_sum_from_index_count( 400, 423293393) );
+//!         b.iter( || get_incremental_sum_from_index_count( black_box(303), black_box(23423439)) );
+//!     })
+//! }
+//!
+//! // run these 2 to get the criterion bench
+//! // cargo bench
+//! // criterion_group!( bench_name, crit_bench );
+//! // criterion_main!( bench_name );
 //! ```
 //!
+//! <br><br>
 //! ### Generate Templates for specific popular projects
 //! - Cargo generate is another useful crate that allows us to make default template for
-//! specific popular packages that are available from the internet. To get a good feel of this 
+//! specific popular packages that are available from the internet. To get a good feel of this
 //! create, have a look at some of the commands below
 //! ```text
 //! // ensure git is setup 1st
 //! cargo generate --git git@github.com:rustwasm/wasm-pack-template.git --name mywasm
 //!
-//! // We could also setup git ssh and for ssh using 
+//! // Generate a project with interactive prompts
+//! cargo generate --git https://github.com/username/mytemplate.git
+//!
+//! // We could also setup git ssh and for ssh using
 //! ~/.gitconfig
 //! [url "ssh://git@github.com/"]
 //! insteadOf = https://github.com/
@@ -116,8 +169,8 @@
 //! for our project and this is a good stepping stone to start something that we are not
 //! familiar with, not to mention, have a checklist of what could be available for our
 //! project. It scaffolds new projects by leveraging pre-existing git repositories.
-//! Have a look the the documentation online for 
+//! Have a look the the documentation online for
 //! [`Cargo Generate`](https://cargo-generate.github.io/cargo-generate/usage/git-over-ssh.html)
 //!
 //!
-#![allow(clippy::doc_lazy_continuation)]  // just to allow next line when we use -
+#![allow(clippy::doc_lazy_continuation)] // just to allow next line when we use -
